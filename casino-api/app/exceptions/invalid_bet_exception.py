2702bd43244107b513dde7bf2225d9a56480722b
barbaracalderon <bcalderoni.ti@gmail.com>
2024-06-17 03:12:35 -0300

    refactor: clean exceptions

--- casino-api/app/exceptions/invalid_bet_exception.py
+++ casino-api/app/exceptions/invalid_bet_exception.py
@@ -2,4 +2,7 @@ from fastapi import HTTPException
 
 class InvalidBetException(HTTPException):
     def __init__(self, value_bet: float):
-        super().__init__(status_code=400, detail=f"Bet value of {value_bet} is not valid.")
+        super().__init__(
+            status_code=400, 
+            detail=f"Bet value of {value_bet} is not valid."
+            )
--- casino-api/app/exceptions/player_not_found_exception.py
+++ casino-api/app/exceptions/player_not_found_exception.py
@@ -2,5 +2,7 @@ from fastapi import HTTPException
 
 class PlayerNotFoundException(HTTPException):
     def __init__(self, player_id: int):
-        detail = f"Player with id {player_id} not found"
-        super().__init__(status_code=404, detail=detail)
+        super().__init__(
+            status_code=404, 
+            detail=f"Player with id {player_id} not found."
+            )
