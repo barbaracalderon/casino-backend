52c7a79dbf910410163ccdaf36dc4f5976f5826c
barbaracalderon <bcalderoni.ti@gmail.com>
2024-06-18 02:52:50 -0300

    feat: Swagger title, description, version, contact and license

--- casino-api/app/main.py
+++ casino-api/app/main.py
@@ -5,7 +5,20 @@ from app.routes.transaction_route import router as transaction_router
 from app.exceptions.player_not_found_exception import PlayerNotFoundException
 
 
-app = FastAPI()
+app = FastAPI(
+    title="Casino API",
+    description="This is a REST API to manage players, balances and transactions of a Casino.",
+    version="1.0.0",
+    contact={
+        "name": "Barbara Calderon",
+        "url": "http://www.github.com/barbaracalderon",
+        "email": "bcalderoni.ti@gmail.com",
+    },
+    license_info={
+        "name": "MIT License",
+        "url": "https://opensource.org/licenses/MIT",
+    },
+)
 
 
 app.include_router(player_router, prefix="/players", tags=["players"])
