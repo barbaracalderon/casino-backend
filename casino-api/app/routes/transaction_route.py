f6a754ca5eab21f849a1def5e70271b81beeb9ba
barbaracalderon <bcalderoni.ti@gmail.com>
2024-06-17 23:35:57 -0300

    feat: endpoint for /players/<player_id>/history

--- casino-api/app/routes/transaction_route.py
+++ casino-api/app/routes/transaction_route.py
@@ -22,7 +22,6 @@ from app.exceptions.invalid_bet_exception import InvalidBetException
 from app.exceptions.invalid_win_exception import InvalidWinException
 from app.exceptions.already_cancelled_exception import AlreadyCancelledException
 from fastapi import HTTPException
-import logging
 
 
 router = APIRouter()
@@ -199,3 +198,29 @@ def rollback_transaction(transaction: TransactionCancelled, db: Session = Depend
         raise HTTPException(status_code=e.status_code, detail=str(e.detail))
     
 
+@router.get("/{player_id}/history")
+def get_player_transaction_history(player_id: int, db: Session = Depends(get_db_session)):
+    transaction_service = get_transaction_service(db)
+    player_service = get_player_service(db)
+    
+    try:
+        player_service.get_player(db=db, player_id=player_id)
+        transactions_by_player_id = transaction_service.get_transactions_by_player_id(db=db, player_id=player_id)
+        
+        history = []
+        for transaction in transactions_by_player_id:
+            history.append({
+                "txn_uuid": transaction.txn_uuid,
+                "type": "bet" if transaction.value_bet > 0 and transaction.value_win == 0 else "win",
+                "value": transaction.value_bet if transaction.value_bet > 0 and transaction.value_win == 0 else transaction.value_win,
+                "rolled_back": transaction.rolled_back if transaction.value_bet > 0 and transaction.value_win == 0 else False
+            })
+        
+        return {
+            "player": player_id,
+            "history": history
+        }
+    except PlayerNotFoundException as e:
+        raise HTTPException(status_code=e.status_code, detail=str(e.detail))
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Internal Server Error")
